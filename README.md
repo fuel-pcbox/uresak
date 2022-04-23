d6b257b0ed9196f86ddcd26094c009e17f835c68
qeeg <mszoopers@protonmail.com>
2022-04-23 14:16:32 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+Uresak
+======
+
+Uresak is the Ultimate Reverse Engineering Swiss Army Knife.
+
+It aims to be a Ghidra equivalent with an emulating debugger to allow you to
+reverse even the most spaghetti-coded applications with ease.
+
+Architectures
+=============
+- [ ] NMOS 6502
+- [ ] x86-16
+
+Binary formats
+==============
+- [ ] Raw
+- [ ] NES 2.0
+- [ ] COM executable
+- [ ] MS-DOS EXE
+
+Environments
+============
+- [ ] PC BIOS
+- [ ] Nintendo Entertainment System/Famicom
+- [ ] MS-DOS COM
+- [ ] Plain MS-DOS EXE
+
+Features
+========
+- [ ] Disassembler
+- [ ] Decompiler to C-like pseudocode
+- [ ] Emulating debugger
+- [ ] Hex editor
+- [ ] Assembler
---
+++ src/core/cpu.h
@@ -0,0 +1,67 @@
+#include "common.h"
+
+struct cpu_register
+{
+    u64 size;
+    enum class type
+    {
+        INTEGER,
+        FLOAT,
+        VECTOR_INT,
+        VECTOR_FLOAT,
+        CONSTANT,
+    } type;
+    std::string name;
+};
+
+struct uop
+{
+    cpu_register dest;
+    std::vector<cpu_register> sources;
+    std::string mnemonic;
+    enum class type
+    {
+        ADD,
+        SUB,
+        MUL,
+        DIV,
+        MOV,
+        AND,
+        OR,
+        XOR,
+        SQRT,
+        HLT,
+    } type;
+
+    static constexpr u64 FLAG_STEP_OUT = 1;
+    static constexpr u64 FLAG_STEP_OVER = 2;
+    static constexpr u64 FLAG_NORETURN = 4;
+    static constexpr u64 FLAG_INTERRUPTIBLE = 8;
+
+    u64 flags;
+    u64 cycles;
+};
+
+struct cpu_instruction
+{
+    u64 length;
+    std::vector<u8> bytes;
+    std::vector<cpu_register> dests;
+    std::vector<cpu_register> sources;
+    std::string mnemonic;
+
+    static constexpr u64 FLAG_STEP_OUT = 1;
+    static constexpr u64 FLAG_STEP_OVER = 2;
+    static constexpr u64 FLAG_NORETURN = 4;
+    static constexpr u64 FLAG_INTERRUPTIBLE = 8;
+
+    u64 flags;
+    u64 cycles;
+
+    virtual void run() = 0;
+};
+
+struct cpu
+{
+    virtual void run_instr() = 0;
+};
